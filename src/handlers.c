bef7d93c6c0f9489288d807f4d0bf4d8ed2be676
addonovan <austin@addonovan.com>
2018-02-10 20:57:48 -0600

    Convert handlers.c

--- src/handlers.c
+++ src/handlers.c
@@ -9,30 +9,23 @@
 #include <stdio.h>
 #include "handlers.h"
 
-handler_t* handler_create( void ( *handler )( int ) )
+void handler_init( handler_t* this, void ( *sig_handler )( int ) )
 {
-  handler_t* this = calloc( 1, sizeof( handler_t ) );
-  this->action.sa_handler = handler;
+  this->action.sa_handler = sig_handler;
 
   if ( sigaction( SIGINT, &this->action, NULL ) < 0 )
   {
     perror( "sigaction: " );
-    free( this );
-    return NULL;
   }
   
   if ( sigaction( SIGTSTP, &this->action, NULL ) < 0 )
   {
     perror( "sigaction: " );
-    free( this );
-    return NULL;
   }
-
-  return this;
 }
 
-void handler_free( handler_t* this )
+void handler_destroy( handler_t* this );
 {
-  free( this );
+  // TODO unregister for signal handlers
 }
 
